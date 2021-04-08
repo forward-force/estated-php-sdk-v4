00832f3cecf396887ab331160a5378a7654a42bb
Teodor Talov <teodor@forward-force.com>
2021-04-08 15:29:31 -0400

    Removed unused code

--- src/Estated.php
+++ src/Estated.php
@@ -4,9 +4,6 @@ namespace ForwardForce\Estated;
 
 use ForwardForce\Estated\Entities\Property;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Psr7\Message;
-use GuzzleHttp\Client as GuzzleHttpClient;
 
 class Estated
 {
@@ -20,40 +17,6 @@ class Estated
     public function __construct(string $token)
     {
         $this->token = $token;
-    }
-
-    /**
-     * Property API Overview
-     * @param string $streetAddress Field Street Address (REQUIRED)
-     * @param string $city Field City (REQUIRED)
-     * @param string $state Field State (REQUIRED)
-     * @param string|null $zipCode Field Zip Code (OPTIONAL)
-     * @deprecated
-     * @return array
-     * @throws GuzzleException
-     */
-    public function generalData(string $streetAddress, string $city, string $state, ?string $zipCode): array
-    {
-        try {
-            $options = [
-                'token' => $this->token,
-                'street_address' => $streetAddress,
-                'city' => $city,
-                'state' => $state,
-                'zip_code' => $zipCode
-            ];
-            $response = $this->client->get(self::BASE_URL . '/property?' . http_build_query($options));
-        } catch (RequestException $e) {
-            $error['request'] = Message::toString($e->getRequest());
-
-            if ($e->hasResponse()) {
-                $error['response'] = Message::toString($e->getResponse());
-            }
-
-            return $error;
-        }
-
-        return json_decode($response->getBody()->getContents() ?? [], true);
     }
 
     /**
